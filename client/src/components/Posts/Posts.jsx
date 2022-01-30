e38a8dd106c824a139b1a2124eef59ed1daa72d8
Francisco Sabino <fdasabino@gmail.com>
2022-01-30 17:50:57 +0100

    added basic ui

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
@@ -12,13 +12,16 @@ const Posts = () => {
 
   const classes = useStyles();
 
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
